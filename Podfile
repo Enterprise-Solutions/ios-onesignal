e5dbe839ff2990b5fac8c1813d8b89795d291f39
macbook ES <>
2019-08-14 16:24:58 -0400

    first commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+target 'Test' do
+  pod 'OneSignal', '>= 2.6.2', '< 3.0'
+end
+
+target 'OneSignalNotificationServiceExtension' do
+  pod 'OneSignal', '>= 2.6.2', '< 3.0'
+end
---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Enterprise Solutions on 8/14/19.
+//  Copyright © 2019 Enterprise Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
